f2af6c957e4f111b56c3984b7f6d93bf9f615383
NoelP <nmjj.populiers@gmail.com>
2023-09-12 11:58:17 +0200

    Create tailwind.config.js

---
+++ tailwind.config.js
@@ -0,0 +1,34 @@
+tailwind.config = {
+  theme: {
+    extend: {
+      animation: {
+        typing: 'typing 2s steps(6), blink 1s infinite',
+      },
+      keyframes: {
+        typing: {
+          from: {
+            width: '0'
+          },
+          to: {
+            width: '6ch'
+          },
+        },
+        blink: {
+          from: {
+            'border-right-color': 'transparent'
+          },
+          to: {
+            'border-right-color': 'black'
+          },
+        },
+      },
+      fontFamily: {
+        satoshi: ['Satoshi', 'sans-serif'],
+        inter: ['Inter', 'sans-serif'],
+      },
+      colors: {
+        'primary-orange': '#FF5722',
+      },
+    },
+  },
+}
